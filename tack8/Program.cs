baea0b1d924fac1c26b9d141b66c9e1460466507
Pakhomov-AV <pahomov93@mail.ru>
2022-10-13 18:56:55 +0400

    task8

--- tack8/Program.cs
+++ tack8/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число (N),
+//а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine (" Введите число ");
+int number = int.Parse(Console.ReadLine());
+
+int i = 0;
+while ( i<number-1)
+{
+i = i+2;
+Console.Write (i+ " ");
+}
